88d4267fe7ecdc724142faad76c49c064078ae99
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-04-23 16:53:50 -0500

    Created a line graph for monthly expenditures

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,18 +2,17 @@ import React from "react";
 import TotalExpensesSection from "../components/TotalExpensesSection";
 import Header from "../components/Header";
 import BarGraphSection from "../components/BarGraphSection";
+import MonthlyExpenditure from "../components/MonthlyExpenditure";
 
 
 function Dashboard() {
   return (
-    <div className="dark-bg grid min-h-screen grid-rows-[0.2fr,3fr,2fr] grid-cols-1 md:grid-cols-4 md:grid-rows-[0.2fr,3fr,2fr] gap-2 p-4">
+    <div className="dark-bg grid min-h-screen grid-rows-[0.2fr,3fr,3fr] grid-cols-1 md:grid-cols-4 md:grid-rows-[0.2fr,3fr,3fr] gap-2 p-4">
       <Header/>
       <TotalExpensesSection/>
       <BarGraphSection/>
-
-      <footer className="md:col-span-4 bg-slate-800 rounded-xl p-6 shadow-lg">
-        Footer Content
-      </footer>
+      <MonthlyExpenditure/>
+     
     </div>
   );
 }
